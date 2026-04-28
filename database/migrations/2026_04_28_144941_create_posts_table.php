fd1308a06cd9fa44f743c4a35ad4e316f88de64c
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-04-29 02:24:43 +0300

    Добавление сидера админки

--- database/migrations/2026_04_28_144941_create_posts_table.php
+++ database/migrations/2026_04_28_144941_create_posts_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps('published_at')->nullable();
+            $table->timestamp('published_at')->nullable();
             $table->string('title');
             $table->text('content');
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
